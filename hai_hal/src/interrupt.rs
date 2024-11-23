4ef1bc45931b4044da139606c00e9bd21573473d
Kira ♡ <38864051+KiraCoding@users.noreply.github.com>
2024-11-23 12:37:38 +0100

    wip

--- hai_hal/src/interrupt.rs
+++ hai_hal/src/interrupt.rs
@@ -3,11 +3,13 @@ use core::arch::asm;
 /// Enable interrupts.
 #[inline(always)]
 pub fn enable() {
+    // SAFETY: This is only safe if the CPU is in a privileged mode that allows writing to the `DAIF` register.
     #[cfg(target_arch = "aarch64")]
     unsafe {
         asm!("msr daifclr, #2", options(nomem, nostack, preserves_flags))
     };
 
+    // SAFETY: This is only safe if the code runs in supervisor mode, allowing access to the `sstatus` register.
     #[cfg(target_arch = "riscv64")]
     unsafe {
         asm!(
@@ -16,6 +18,7 @@ pub fn enable() {
         )
     };
 
+    // SAFETY: This is only safe if the code is running in ring 0 (kernel mode), where setting the interrupt flag is permitted.
     #[cfg(target_arch = "x86_64")]
     unsafe {
         asm!("sti", options(nomem, nostack, preserves_flags))
@@ -25,11 +28,13 @@ pub fn enable() {
 /// Disable interrupts.
 #[inline(always)]
 pub fn disable() {
+    // SAFETY: This is only safe if the CPU is in a privileged mode that allows writing to the `DAIF` register.
     #[cfg(target_arch = "aarch64")]
     unsafe {
         asm!("msr daifset, #2", options(nomem, nostack, preserves_flags))
     };
 
+    // SAFETY: This is only safe if the code runs in supervisor mode, allowing access to the `sstatus` register.
     #[cfg(target_arch = "riscv64")]
     unsafe {
         asm!(
@@ -38,6 +43,7 @@ pub fn disable() {
         )
     };
 
+    // SAFETY: This is only safe if the code is running in ring 0 (kernel mode), where clearing the interrupt flag is allowed.
     #[cfg(target_arch = "x86_64")]
     unsafe {
         asm!("cli", options(nomem, nostack, preserves_flags));
@@ -47,16 +53,19 @@ pub fn disable() {
 /// Places the CPU into a low-power state, awaiting an interrupt.
 #[inline(always)]
 pub fn wait() {
+    // SAFETY: This is only safe if the CPU is in a mode where waiting for an interrupt is allowed.
     #[cfg(target_arch = "aarch64")]
     unsafe {
         asm!("wfi", options(nomem, nostack, preserves_flags));
     }
 
+    // SAFETY: This is only safe if the CPU is in a mode where waiting for an interrupt is allowed.
     #[cfg(target_arch = "riscv64")]
     unsafe {
         asm!("wfi", options(nomem, nostack, preserves_flags));
     }
 
+    // SAFETY: This is only safe if the code is running in ring 0 (kernel mode), where halting the CPU is permitted.
     #[cfg(target_arch = "x86_64")]
     unsafe {
         asm!("hlt", options(nomem, nostack, preserves_flags));
