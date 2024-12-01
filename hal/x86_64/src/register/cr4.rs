797fdd3fe04214e866ce67bee73fb11f122f6215
Kira ♡ <38864051+KiraCoding@users.noreply.github.com>
2024-12-01 18:01:36 +0100

    wip

--- hal/x86_64/src/register/cr4.rs
+++ hal/x86_64/src/register/cr4.rs
@@ -5,61 +5,61 @@ bitflags! {
     #[repr(transparent)]
     #[derive(Debug)]
     pub struct CR4: u64 {
-        /// Virtual-8086 Mode Extensions
+        /// **V**irtual-8086 **M**ode **E**xtensions
         /// - If set, enables support for the virtual interrupt flag (VIF) in virtual-8086 mode.
         const VME = 1 << 0;
 
-        /// Protected-mode Virtual Interrupts
+        /// **P**rotected-mode **V**irtual **I**nterrupts
         /// - If set, enables support for the virtual interrupt flag (VIF) in protected mode.
         const PVI = 1 << 1;
 
-        /// Time Stamp Disable
-        /// - If set, RDTSC instruction can only be executed when in ring 0,
-        /// otherwise RDTSC can be used at any privilege level.
+        /// **T**ime **S**tamp **D**isable
+        /// - If set, `RDTSC` instruction can only be executed when in ring 0,
+        /// otherwise `RDTSC` can be used at any privilege level.
         const TSD = 1 << 2;
 
-        /// Debugging Extensions
+        /// **D**ebugging **E**xtensions
         /// - If set, enables debug register based breaks on I/O space access.
         const DE = 1 << 3;
 
-        /// Page Size Extension
+        /// **Page** **Size** **Extension**
         /// - If set, enables 32-bit paging mode to use 4 MiB huge pages in addition to 4 KiB pages.
         /// - If PAE is enabled or the processor is in x86-64 long mode this bit is ignored.
         const PSE = 1 << 4;
 
-        /// Physical Address Extension
+        /// **P**hysical **A**ddress **E**xtension
         /// - If set, changes page table layout to translate 32-bit
         /// virtual addresses into extended 36-bit physical addresses.
         const PAE = 1 << 5;
 
-        /// Machine Check Exception
+        /// **M**achine **C**heck **E**xception
         /// - If set, enables machine check interrupts to occur.
         const MCE = 1 << 6;
 
-        /// Page Global Enabled
+        /// **P**age **G**lobal **E**nabled
         /// - If set, address translations (PDE or PTE records) may be shared between address spaces.
         const PGE = 1 << 7;
 
-        /// Performance-Monitoring Counter enable
-        /// - If set, RDPMC can be executed at any privilege level, else RDPMC can only be used in ring 0.
+        /// **P**erformance-Monitoring **C**ounter **e**nable
+        /// - If set, `RDPMC` can be executed at any privilege level, else RDPMC can only be used in ring 0.
         const PCE = 1 << 8;
 
-        /// Operating system support for FXSAVE and FXRSTOR instructions
+        /// **O**perating **S**ystem support for **FXS**AVE and **FXR**STOR instructions
         /// - If set, enables Streaming SIMD Extensions (SSE) instructions and fast FPU save & restore.
         const OSFXSR = 1 << 9;
 
-        /// Operating System Support for Unmasked SIMD Floating-Point Exceptions
+        /// **O**perating **System** Support for Unmasked SIMD Floating-Point Exceptions
         /// - If set, enables unmasked SSE exceptions.
         const OSXMMEXCPT = 1 << 10;
 
-        /// User-Mode Instruction Prevention
-        /// - If set, the SGDT, SIDT, SLDT, SMSW and STR instructions cannot be executed if CPL > 0.
+        /// **U**ser-**M**ode **I**nstruction **P**revention
+        /// - If set, the `SGDT`, `SIDT`, `SLDT`, `SMSW` and `STR` instructions cannot be executed if CPL > 0.
         const UMIP = 1 << 11;
 
-        /// Virtual Machine Extensions Enable
+        /// **V**irtual **M**achine **E**xtensions **E**nable
         const VMXE = 1 << 13;
 
-        /// Safer Mode Extensions Enable
+        /// **S**afer **M**ode E**x**tensions **E**nable
         const SMXE = 1 << 14;
 
         /// FSGSBASE Enable
@@ -73,22 +73,22 @@ bitflags! {
         /// XSAVE and Processor Extended States Enable
         const OSXSAVE = 1 << 18;
 
-        /// Supervisor Mode Execution Protection Enable
+        /// **S**upervisor **M**ode **E**xecution **P**rotection Enable
         /// - If set, execution of code in a higher ring generates a fault.
         const SMEP = 1 << 20;
 
-        /// Supervisor Mode Access Prevention Enable
+        /// **S**upervisor **M**ode **A**ccess **P**revention Enable
         /// - If set, access of data in a higher ring generates a fault.
         const SMAP = 1 << 21;
 
-        /// Protection Key Enable
+        /// **P**rotection **K**ey **E**nable
         const PKE = 1 << 22;
 
-        /// Control-flow Enforcement Technology
+        /// **C**ontrol-flow **E**nforcement **T**echnology
         /// - If set, enables control-flow enforcement technology.
         const CET = 1 << 23;
 
-        /// Enable Protection Keys for Supervisor-Mode Pages
+        /// Enable **P**rotection **K**eys for **S**upervisor-Mode Pages
         /// - If set, each supervisor-mode linear address is associated
         /// with a protection key when 4-level or 5-level paging is in use
         const PKS = 1 << 24;
