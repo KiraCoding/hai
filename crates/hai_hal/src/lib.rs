f01ec0329647024caea1a38ac822f35488fa1e05
Kira ♡ <38864051+KiraCoding@users.noreply.github.com>
2024-12-06 15:41:47 +0100

    wip

--- crates/hai_hal/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![no_std]
-#![warn(clippy::all)]
-#![warn(clippy::nursery)]
-#![warn(clippy::pedantic)]
-#![feature(abi_x86_interrupt, abi_riscv_interrupt)]
-
-pub mod memory;
--- crates/hai_hal/src/memory.rs
+++
@@ -1,8 +0,0 @@
-pub struct MemoryMap {}
-
-#[cfg(all(target_os = "uefi", feature = "uefi"))]
-impl From<uefi::mem::memory_map::MemoryMapOwned> for MemoryMap {
-    fn from(_value: uefi::mem::memory_map::MemoryMapOwned) -> Self {
-        todo!()
-    }
-}
