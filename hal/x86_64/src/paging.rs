adee77fe8c9aa013eb273f9789b59a35cdc8b299
lina♡ <38864051+KiraCoding@users.noreply.github.com>
2024-12-22 18:21:54 +0100

    wip

--- hal/x86_64/src/paging.rs
+++ hal/x86_64/src/paging.rs
@@ -1,12 +1,12 @@
 use crate::register::CR0;
 
-/// Enables paging
+/// Enables paging.
 #[inline]
 pub fn enable() {
     unsafe { CR0::update(|cr0| *cr0 |= CR0::PG) };
 }
 
-/// Disables paging
+/// Disables paging.
 #[inline]
 pub fn disable() {
     unsafe { CR0::update(|cr0| *cr0 &= !CR0::PG) };
