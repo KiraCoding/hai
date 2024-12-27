bb0046d9513e14670eac42da5c8f1b6208bfb5fd
lina♡ <38864051+KiraCoding@users.noreply.github.com>
2024-12-27 21:07:01 +0100

    wip

--- scripts/qemu.rs
+++ scripts/qemu.rs
@@ -7,29 +7,30 @@
 
 use std::env::args;
 use std::fs::{copy, create_dir_all};
+use std::io::{Error, ErrorKind, Result};
 use std::process::Command;
 
 const CODE: &str = "./tools/ovmf/x86_64/code.fd";
 const VARS: &str = "./tools/ovmf/x86_64/vars.fd";
 
-fn main() {
-    let target = &args().nth(1).unwrap();
+fn main() -> Result<()> {
+    let target = &args()
+        .nth(1)
+        .ok_or_else(|| Error::new(ErrorKind::InvalidInput, "Missing target argument"))?;
 
     let build = Command::new("cargo")
         .args(&["build", "--profile", "qemu", "--target", target])
-        .status()
-        .unwrap();
+        .status()?;
 
     if !build.success() {
-        return;
+        return Err(Error::new(ErrorKind::Other, "Build failed"));
     }
 
-    create_dir_all("./target/x86_64-unknown-uefi/qemu/esp/efi/boot").unwrap();
+    create_dir_all("./target/x86_64-unknown-uefi/qemu/esp/efi/boot")?;
     copy(
         "./target/x86_64-unknown-uefi/qemu/kernel.efi",
         "./target/x86_64-unknown-uefi/qemu/esp/efi/boot/bootx64.efi",
-    )
-    .unwrap();
+    )?;
 
     Command::new("qemu-system-x86_64")
         .args(&[
@@ -40,6 +41,7 @@ fn main() {
             "-drive",
             "format=raw,file=fat:rw:./target/x86_64-unknown-uefi/qemu/esp",
         ])
-        .status()
-        .unwrap();
+        .status()?;
+
+    Ok(())
 }
