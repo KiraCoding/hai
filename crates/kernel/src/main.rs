3c43089fe8c9267359b84f00b10c35bde5fafc2c
lina♡ <38864051+KiraCoding@users.noreply.github.com>
2025-01-10 19:31:15 +0100

    wip

--- crates/kernel/src/main.rs
+++ crates/kernel/src/main.rs
@@ -4,13 +4,16 @@
 #[uefi::entry]
 #[cfg(target_os = "uefi")]
 fn efi_main() -> uefi::Status {
-    use uefi::{
-        boot::{exit_boot_services, MemoryType},
-        helpers::init,
-        mem::memory_map::MemoryMap,
-    };
+    use uefi::boot::{exit_boot_services, MemoryType};
+    use uefi::mem::memory_map::MemoryMap;
+    use uefi::runtime::get_time;
 
-    init().unwrap();
+    fn efi_time() -> u64 {
+        let time = get_time().unwrap();
+        time.hour() as u64 * 3600 + time.minute() as u64 * 60 + time.second() as u64
+    }
+
+    let time_start = efi_time();
 
     let mmap = unsafe { exit_boot_services(MemoryType::LOADER_DATA) };
 
@@ -22,15 +25,27 @@ fn efi_main() -> uefi::Status {
         _ => (),
     });
 
-    kernel_main();
+    let time = efi_time() - time_start;
+
+    kernel_main(BootInfo {
+        kind: Kind::Uefi { time },
+    });
 
     uefi::Status::SUCCESS
 }
 
 #[inline(never)]
-fn kernel_main() {
-    //! TODO: setup idt before enabling interrupts
+fn kernel_main(info: BootInfo) {
+    // TODO: setup idt before enabling interrupts
     // cpu64::interrupt::enable();
 
     loop {}
+}
+
+pub struct BootInfo {
+    kind: Kind,
+}
+
+pub enum Kind {
+    Uefi { time: u64 },
 }
