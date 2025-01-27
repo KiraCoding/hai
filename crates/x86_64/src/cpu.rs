c71eb30f1184f0c79f76394d16b1e3f91f44915a
lina♡ <38864051+KiraCoding@users.noreply.github.com>
2025-01-27 07:20:40 +0100

    wip

---
+++ crates/x86_64/src/cpu.rs
@@ -0,0 +1,5 @@
+use core::arch::asm;
+
+pub fn identify() {
+    unsafe { asm!("") }
+}
--- crates/x86_64/src/lib.rs
+++ crates/x86_64/src/lib.rs
@@ -2,6 +2,7 @@
 #![feature(abi_x86_interrupt)]
 
 pub mod address;
+pub mod cpu;
 pub mod interrupt;
 pub mod paging;
 pub mod register;
